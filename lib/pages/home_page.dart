92447866ed667c9b8fa9c2f6e4e812ae795fdab4
Riki Sanjaya <imaderikiwidiastanasanjaya@gmail.com>
2024-06-25 21:52:05 +0800

    add drawer

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:music_player/components/my_drawer.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,6 +11,10 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      appBar: AppBar(title: const Text("P L A Y L I S T")),
+      drawer: const MyDrawer(),
+    );
   }
 }
