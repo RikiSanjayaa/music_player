5b664556d65d9a0235572aafabc730fbb3940ba5
Riki Sanjaya <imaderikiwidiastanasanjaya@gmail.com>
2024-06-26 17:16:16 +0800

    change UI from push to slide with bottom navbar when changing page between playlist and song page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -13,6 +13,9 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+  final PageController _pageController = PageController();
+
   // get the playlist provider
   late final dynamic playlistProvider;
 
@@ -22,50 +25,90 @@ class _HomePageState extends State<HomePage> {
 
     // get playlist provider
     playlistProvider = Provider.of<PlaylistProvider>(context, listen: false);
+
+    // add listener to page controller
+    _pageController.addListener(() {
+      int nextPage = _pageController.page!.round();
+      if (nextPage != _selectedIndex) {
+        setState(() {
+          _selectedIndex = nextPage;
+        });
+      }
+    });
   }
 
-  // go to song
-  void goToSong(int songIndex) {
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+    _pageController.animateToPage(index,
+        duration: const Duration(milliseconds: 300), curve: Curves.easeInOut);
+  }
+
+  // play song
+  void playSong(int songIndex) {
     // update current song index
     playlistProvider.currentSongIndex = songIndex;
+    _onItemTapped(1);
+  }
 
-    // navigate to song page
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (context) => const SongPage(),
-      ),
-    );
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.playlist_play_rounded), label: 'Playlist'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.music_note), label: "Now Playing"),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        selectedItemColor: Theme.of(context).colorScheme.onBackground,
+        unselectedItemColor: Theme.of(context).colorScheme.primary,
+      ),
       backgroundColor: Theme.of(context).colorScheme.background,
-      appBar: AppBar(title: const Text("P L A Y L I S T")),
+      appBar: AppBar(title: const Text("My Music Player")),
       drawer: const MyDrawer(),
-      body: Consumer<PlaylistProvider>(
-        builder: (context, value, child) {
-          // get the playlist
-          final List<Song> playlist = value.playlist;
+      body: PageView(
+        controller: _pageController,
+        children: [
+          // original home page
+          Consumer<PlaylistProvider>(
+            builder: (context, value, child) {
+              // get the playlist
+              final List<Song> playlist = value.playlist;
 
-          // return list view UI
-          return ListView.builder(
-            itemCount: playlist.length,
-            itemBuilder: (context, index) {
-              // get individual song
-              final Song song = playlist[index];
+              // return list view UI
+              return ListView.builder(
+                itemCount: playlist.length,
+                itemBuilder: (context, index) {
+                  // get individual song
+                  final Song song = playlist[index];
 
-              // return list tile UI
-              return ListTile(
-                title: Text(song.songName),
-                subtitle: Text(song.artistName),
-                leading: Image.asset(song.albumArtImagePath),
-                onTap: () => goToSong(index),
+                  // return list tile UI
+                  return ListTile(
+                    title: Text(song.songName),
+                    subtitle: Text(song.artistName),
+                    leading: Image.asset(song.albumArtImagePath),
+                    onTap: () {
+                      playSong(index);
+                    },
+                  );
+                },
               );
             },
-          );
-        },
+          ),
+
+          // second page
+          const SongPage()
+        ],
       ),
     );
   }
